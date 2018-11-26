613d0d791186463cfcd9f7dc98ffbfa59054b0cf
Stijn van Nieulande <stijn.van.nieulande@gmail.com>
2018-11-26 16:56:59 +0100

    style fixes

--- scripts/config.js
+++ scripts/config.js
@@ -1,37 +1,37 @@
-const { join } = require('path');
-const { promisify } = require('util');
-const { readdir } = require('fs');
+const {join} = require('path');
+const {promisify} = require('util');
+const {readdir} = require('fs');
 
 const getSlackDirectory = () => {
-  switch (process.platform) {
-    case 'win32':
-      return join(process.env.APPDATA, '..', 'Local', 'slack');
-    case 'aix':
-    case 'android':
-    case 'cygwin':
-    case 'darwin':
-    case 'freebsd':
-    case 'linux':
-    case 'openbsd':
-    case 'sunos':
-    default:
-      throw new Error('Your platform is not currently supported');
-  }
-} 
+    switch (process.platform) {
+        case 'win32':
+            return join(process.env.APPDATA, '..', 'Local', 'slack');
+        case 'aix':
+        case 'android':
+        case 'cygwin':
+        case 'darwin':
+        case 'freebsd':
+        case 'linux':
+        case 'openbsd':
+        case 'sunos':
+        default:
+            throw new Error('Your platform is not currently supported');
+    }
+}
 
 const getSlackStaticDirectory = async () => {
-  let dir = getSlackDirectory();
-  
-  const dirs = (await promisify(readdir)(dir))
-    .filter(f => f.toLowerCase().startsWith('app-'))
-    .filter(f => !f.toLowerCase().endsWith('.ico'))
-    .sort();
+    let dir = getSlackDirectory();
 
-  return join(dir, dirs[dirs.length - 1], 'resources', 'app.asar.unpacked', 'src', 'static');
+    const dirs = (await promisify(readdir)(dir))
+        .filter(f => f.toLowerCase().startsWith('app-'))
+        .filter(f => !f.toLowerCase().endsWith('.ico'))
+        .sort();
+
+    return join(dir, dirs[dirs.length - 1], 'resources', 'app.asar.unpacked', 'src', 'static');
 }
 
 module.exports = {
-  enableDevTools: true,
-  devToolsMode: 'docked',
-  getSlackStaticDirectory
+    enableDevTools: true,
+    devToolsMode: 'docked',
+    getSlackStaticDirectory
 }
--- scripts/init-interop.js
+++ scripts/init-interop.js
@@ -1,29 +1,29 @@
 (async function () {
-  const { readFile: _readFile, writeFile } = require('fs');
-  const { join } = require('path');
-  const { promisify } = require('util');
-  const { enableDevTools, devToolsMode, getSlackStaticDirectory } = require('./config');
-  
-  const staticFile = await getSlackStaticDirectory();
+    const {readFile: _readFile, writeFile} = require('fs');
+    const {join} = require('path');
+    const {promisify} = require('util');
+    const {enableDevTools, devToolsMode, getSlackStaticDirectory} = require('./config');
 
-  const readFile = promisify(_readFile);  
-  let interop = (await readFile(join(staticFile, 'ssb-interop.js'))).toString();
+    const staticFile = await getSlackStaticDirectory();
 
-  let begin = interop.indexOf('// %SLACK_DISCORD_THEME_BEGIN%');
-  let end = interop.indexOf('// %SLACK_DISCORD_THEME_END%');
+    const readFile = promisify(_readFile);
+    let interop = (await readFile(join(staticFile, 'ssb-interop.js'))).toString();
 
-  if (begin !== -1 && end !== -1) {
-    begin -= 2;
-    end += 28;
-    interop = interop.substr(0, begin) + interop.substr(end, interop.length - end);
-  }
+    let begin = interop.indexOf('// %SLACK_DISCORD_THEME_BEGIN%');
+    let end = interop.indexOf('// %SLACK_DISCORD_THEME_END%');
 
-  let interopMod = await readFile(join(__dirname, '..', 'resources', 'ssb-interop-mod.txt'));
-  interopMod = interop + interopMod.toString()
-    .replace('%SLACK_DEV_TOOLS%', enableDevTools)
-    .replace('%SLACK_DEV_TOOLS_MODE%', devToolsMode)
-    .replace('%SLACK_ROOT%', staticFile.replace(/\\/g, '\\\\'))
-    .replace('%SLACK_CSS_FILENAME%', 'main.css');
+    if (begin !== -1 && end !== -1) {
+        begin -= 2;
+        end += 28;
+        interop = interop.substr(0, begin) + interop.substr(end, interop.length - end);
+    }
 
-  await promisify(writeFile)(join(staticFile, 'ssb-interop.js'), interopMod);
+    let interopMod = await readFile(join(__dirname, '..', 'resources', 'ssb-interop-mod.txt'));
+    interopMod = interop + interopMod.toString()
+        .replace('%SLACK_DEV_TOOLS%', enableDevTools)
+        .replace('%SLACK_DEV_TOOLS_MODE%', devToolsMode)
+        .replace('%SLACK_ROOT%', staticFile.replace(/\\/g, '\\\\'))
+        .replace('%SLACK_CSS_FILENAME%', 'main.css');
+
+    await promisify(writeFile)(join(staticFile, 'ssb-interop.js'), interopMod);
 })();
