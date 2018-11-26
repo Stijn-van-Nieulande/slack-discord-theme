8b1cadd37491c29269b516fb78596507113ca7b5
Stijn van Nieulande <stijn.van.nieulande@gmail.com>
2018-11-26 13:26:43 +0100

    Update dev-script.js

--- dev-script.js
+++ dev-script.js
@@ -1,8 +1,22 @@
+var slackDiscordThemeInjecting = false,
+    injectSlackDiscordTheme = function () {
+        $.ajax({
+            url: 'https://raw.githubusercontent.com/Stijn-van-Nieulande/slack-discord-theme/master/out/main.css',
+            success: function (css) {
+                $("<style></style>").appendTo('head').html(css);
+            }
+        });
+    };
+
 document.addEventListener('DOMContentLoaded', function () {
-    $.ajax({
-        url: 'https://raw.githubusercontent.com/Stijn-van-Nieulande/slack-discord-theme/master/out/main.css',
-        success: function (css) {
-            $("<style></style>").appendTo('head').html(css);
-        }
-    });
+    if (!slackDiscordThemeInjecting) {
+        slackDiscordThemeInjecting = true;
+        injectSlackDiscordTheme();
+    }
+});
+jQuery(document).ready(function () {
+    if (!slackDiscordThemeInjecting) {
+        slackDiscordThemeInjecting = true;
+        injectSlackDiscordTheme();
+    }
 });
